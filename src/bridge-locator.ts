3c541c555ebbeeb8c3221bb4c61c871ef2c00af3
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-10-08 19:20:10 +0100

    chore: refactor resources into folder

--- src/bridge-locator.ts
+++ src/bridge-locator.ts
@@ -1,16 +1,16 @@
-import { getServiceIpWithBonjour } from "./get-service-ip-with-bonjour";
+import { getServiceIpWithBonjour } from "./utils/get-service-ip-with-bonjour";
 import { LocalHueClient } from "./local-hue-client";
 import { ClientConfig } from "./types";
 import { Bridge } from "./bridge";
 import { HueBridge } from "./hue-bridge";
 
 export class BridgeLocator {
-  public static async local(config: ClientConfig): Promise<HueBridge> {
-    const ip = await getServiceIpWithBonjour("hue");
-    const client = new LocalHueClient(ip, config.deviceType, config.username);
+	public static async local(config: ClientConfig): Promise<HueBridge> {
+		const ip = await getServiceIpWithBonjour("hue");
+		const client = new LocalHueClient(ip, config.deviceType, config.username);
 
-    const newConfig = { ...config, username: await client.getUsername() };
+		const newConfig = { ...config, username: await client.getUsername() };
 
-    return new Bridge(client, newConfig);
-  }
+		return new Bridge(client, newConfig);
+	}
 }
