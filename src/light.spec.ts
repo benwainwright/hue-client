ca74f509bf4b58f52a5acb1d98237eca7cd577a8
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-10-08 16:30:43 +0100

    test: increased test coverage

--- src/light.spec.ts
+++ src/light.spec.ts
@@ -12,4 +12,15 @@ describe("light", () => {
       expect(mockClient.get).toBeCalledWith("/lights/1");
     });
   });
+
+  describe("on", () => {
+    it("calls the client state endpoint with the correct body", async () => {
+      const mockClient = mock<Client>();
+      const light = new Light("1", mock(), mockClient);
+
+      await light.on();
+
+      expect(mockClient.put).toBeCalledWith("/lights/1/state", { on: true });
+    });
+  });
 });
